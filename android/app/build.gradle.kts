0a7bd8f36739a96a66c5782b26ec5faaf224cbe9
mbadrudin170 <mbadrudin170@gmail.com>
2026-05-24 11:22:01 +0000

    m

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,6 +73,7 @@ android {
 dependencies {
     implementation ("androidx.multidex:multidex:2.0.1")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
 
 flutter {
