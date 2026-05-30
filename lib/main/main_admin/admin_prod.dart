ac2936b1182c28039c86cb1fe0f8b7ddb26e21b2
mbadrudin170 <mbadrudin170@gmail.com>
2026-05-30 05:04:09 +0000

    n

--- lib/main/main_admin/admin_prod.dart
+++ lib/main/main_admin/admin_prod.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:wifi/admin/app_admin.dart';
 import 'package:wifi/admin/firebase_option/firebase_option_admin_prod.dart';
 import 'package:wifi/shared/debug/log.dart';
@@ -21,13 +20,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  // Inisialisasi Supabase
-  await Supabase.initialize(
-    url: 'https://zptfgbvloodkgioyeaix.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwdGZnYnZsb29ka2dpb3llYWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzU3NDgsImV4cCI6MjA5NTY1MTc0OH0.YuvZ7zH6iyk8DuFVhBZhDCT-82VBomJSoCgrFPanfns',
-  );
-  Log.info('Inisialisasi Supabase selesai.');
 
   Log.info(' Menginisialisasi Google Mobile Ads SDK...');
   await MobileAds.instance.initialize();
