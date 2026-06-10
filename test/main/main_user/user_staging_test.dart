d91a0de411903385aa1e564e7bac33a10a1d5eeb
mbadrudin170 <mbadrudin170@gmail.com>
2026-06-10 17:29:28 +0000

    n

---
+++ test/main/main_user/user_staging_test.dart
@@ -0,0 +1,10 @@
+// path: test/main/main_user/user_staging_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('user staging', () {
+    testWidgets('Unit Test 1: Tampilan Awal User Staging', (WidgetTester tester) async {
+      // TODO: Implement test
+    });
+  });
+}
---
+++ test/model/customer_model_test.dart
@@ -0,0 +1,10 @@
+// path: test/model/customer_model_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('customer model', () {
+    test('Unit Test 1: Inisialisasi Customer Model', () {
+      // TODO: Implement test
+    });
+  });
+}
