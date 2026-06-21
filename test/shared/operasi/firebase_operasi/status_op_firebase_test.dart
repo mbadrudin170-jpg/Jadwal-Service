b74c673eadf7535b0dbf20ef8e5ad0db2ef548f8
mbadrudin170 <mbadrudin170@gmail.com>
2026-06-21 21:07:26 +0000

    m

--- test/shared/operasi/firebase_operasi/status_op_firebase_test.dart
+++ test/shared/operasi/firebase_operasi/status_op_firebase_test.dart
@@ -1,6 +1,6 @@
 // path: test/shared/operasi/firebase_operasi/status_op_firebase_test.dart
-import 'package:flutter_test/flutter_test.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:wifi/shared/constant/nama_tabel.dart';
 import 'package:wifi/shared/model/status_model.dart';
 import 'package:wifi/shared/operasi/firebase_operasi/status_op_firebase.dart';
@@ -18,7 +18,10 @@ void main() {
     test('01. harus memperbarui status global dengan benar', () async {
       await statusOpFirebase.perbaruiStatusGlobal();
 
-      final doc = await fakeFirestore.collection(NamaTabel.statusGlobal).doc(globalStatusId).get();
+      final doc = await fakeFirestore
+          .collection(NamaTabel.statusGlobal)
+          .doc(globalStatusId)
+          .get();
 
       expect(doc.exists, isTrue);
       expect(doc.data(), isNotNull);
