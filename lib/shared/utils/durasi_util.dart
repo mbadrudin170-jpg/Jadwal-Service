71f52c9520c7fd523658d20497b8775dc43db946
mbadrudin170 <mbadrudin170@gmail.com>
2026-06-24 17:44:03 +0000

    m

--- lib/shared/utils/durasi_util.dart
+++ lib/shared/utils/durasi_util.dart
@@ -4,24 +4,13 @@ import 'package:jiffy/jiffy.dart';
 import 'package:wifi/fitur/paket/enum/tipe_durasi_paket.dart';
 import 'package:wifi/fitur/paket/model/paket_model.dart';
 
-/// Kelas utilitas untuk perhitungan durasi yang konsisten di seluruh aplikasi.
 class DurasiUtil {
   DurasiUtil._();
 
-  /// Menghitung durasi paket dalam satuan menit.
-  /// 
-  /// [paket] Model paket yang akan dihitung durasinya.
-  /// Mengembalikan total durasi dalam menit.
   static int hitungDurasiDalamMenit(PaketModel paket) {
     return _konversiKeMenit(paket.tipe, paket.durasi);
   }
 
-  /// Menghitung durasi total (paket + bonus) dalam menit.
-  /// 
-  /// [paket] Model paket.
-  /// [durasiBonus] Durasi bonus (opsional).
-  /// [tipeBonus] Tipe durasi bonus (opsional).
-  /// Mengembalikan total durasi dalam menit.
   static int hitungTotalDurasiDalamMenit(
     PaketModel paket, {
     int? durasiBonus,
@@ -36,12 +25,6 @@ class DurasiUtil {
     return totalMenit;
   }
 
-  /// Menambahkan durasi ke DateTime tertentu.
-  /// 
-  /// [tanggal] Tanggal awal.
-  /// [tipe] Tipe durasi (menit, jam, hari, bulan).
-  /// [jumlah] Jumlah durasi.
-  /// Mengembalikan DateTime baru setelah penambahan durasi.
   static DateTime tambahDurasi(
     DateTime tanggal,
     TipeDurasiPaket tipe,
@@ -55,16 +38,10 @@ class DurasiUtil {
       case TipeDurasiPaket.days:
         return tanggal.add(Duration(days: jumlah));
       case TipeDurasiPaket.months:
-        // Menggunakan Jiffy untuk perhitungan bulan yang akurat
         return Jiffy.parseFromDateTime(tanggal).add(months: jumlah).dateTime;
     }
   }
 
-  /// Konversi tipe durasi ke jumlah menit.
-  /// 
-  /// [tipe] Tipe durasi.
-  /// [jumlah] Jumlah durasi.
-  /// Mengembalikan total durasi dalam menit.
   static int _konversiKeMenit(TipeDurasiPaket tipe, int jumlah) {
     switch (tipe) {
       case TipeDurasiPaket.minutes:
@@ -74,8 +51,6 @@ class DurasiUtil {
       case TipeDurasiPaket.days:
         return jumlah * 24 * 60;
       case TipeDurasiPaket.months:
-        // Asumsi 1 bulan = 30 hari untuk konversi ke menit
-        // Ini hanya untuk keperluan sorting/perbandingan, bukan perhitungan tanggal aktual
         return jumlah * 30 * 24 * 60;
     }
   }
