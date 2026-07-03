d99c16195df7a56e04013e610383d976a1a6bfff
mbadrudin170 <mbadrudin170@gmail.com>
2026-07-03 13:14:18 +0000

    m

--- lib/fitur/paket/page/form_voucher.dart
+++ lib/fitur/paket/page/form_voucher.dart
@@ -1,9 +1,5 @@
-// path lib/fitur/paket/page/form_voucher.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wifi/shared/debug/log.dart';
-import 'package:wifi/shared/utils/toast_util.dart';
 
 class FormVoucher extends ConsumerStatefulWidget {
   const FormVoucher({super.key});
@@ -13,33 +9,20 @@ class FormVoucher extends ConsumerStatefulWidget {
 }
 
 class _FormVoucherState extends ConsumerState<FormVoucher> {
-  bool _isLoading = true;
-
   @override
   void initState() {
     super.initState();
-    _loadData();
   }
 
-  Future<void> _loadData() async {
-    setState(() => _isLoading = true);
-    try {
-      // panggil repository
-      if (mounted) setState(() => _isLoading = false);
-    } on Exception catch (e, s) {
-      Log.error('Error', e: e, s: s);
-      if (mounted) {
-        setState(() => _isLoading = false);
-        ToastUtil.error(context, 'Gagal memuat data');
-      }
-    }
+  @override
+  void dispose() {
+    // Bersihkan resource di sini jika diperlukan.
+    // Contoh: _repository.dispose(); atau _subscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_isLoading) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
     return Scaffold(
       appBar: AppBar(title: const Text('FormVoucher')),
       body: Container(),
