ee64cd326f3a2bf16bc696f7046e1bd24fca22bd
mbadrudin170 <mbadrudin170@gmail.com>
2026-07-03 16:30:25 +0000

    d

--- lib/fitur/voucher/operasi/voucher_op_firebase.dart
+++ lib/fitur/voucher/operasi/voucher_op_firebase.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wifi/fitur/voucher/model/voucher_model.dart';
+import 'package:wifi/shared/constant/nama_kolom.dart';
 import 'package:wifi/shared/debug/log.dart';
 
 class VoucherOpFirebase {
@@ -11,9 +12,17 @@ class VoucherOpFirebase {
 
   /// Mengambil semua dokumen voucher dari Firestore.
   /// Perhatian: jika data sangat banyak, pertimbangkan pagination.
-  Future<List<VoucherModel>> ambilSemua() async {
+  Future<List<VoucherModel>> ambilSemua({
+    bool tampilkanYangDiarsip = false,
+  }) async {
     try {
-      final snapshot = await _firestore.collection(_koleksiVoucher).get();
+      final snapshot =
+          await (tampilkanYangDiarsip
+                  ? _firestore.collection(_koleksiVoucher)
+                  : _firestore
+                        .collection(_koleksiVoucher)
+                        .where(NamaKolom.dihapus, isEqualTo: false))
+              .get();
       final vouchers = snapshot.docs.map((doc) {
         return VoucherModel.fromFirebase(doc.id, doc.data());
       }).toList();
@@ -27,10 +36,33 @@ class VoucherOpFirebase {
   Future<VoucherModel> tambah({required VoucherModel voucher}) async {
     try {
       final data = voucher.toFirebase();
-      final docRef = await _firestore.collection(_koleksiVoucher).add(data);
-      return voucher.copyWith(id: docRef.id);
+      await _firestore.collection(_koleksiVoucher).doc(voucher.id).set(data);
+      return voucher.copyWith(id: voucher.id);
     } on Exception catch (e, s) {
       Log.error('Error di tambah: $e', e: e, s: s);
+      rethrow;
+    }
+  }
+
+  Future<VoucherModel> perbarui({required VoucherModel voucher}) async {
+    try {
+      final data = voucher.toFirebase();
+      await _firestore.collection(_koleksiVoucher).doc(voucher.id).update(data);
+      return voucher;
+    } on Exception catch (e, s) {
+      Log.error('Error di perbarui: $e', e: e, s: s);
+      rethrow;
+    }
+  }
+
+  Future<void> hapus(String idVoucher) async {
+    try {
+      await _firestore.collection(_koleksiVoucher).doc(idVoucher).update({
+        NamaKolom.dihapus: true,
+        NamaKolom.diarsipkanPada: Timestamp.fromDate(DateTime.now()),
+      });
+    } on Exception catch (e, s) {
+      Log.error('Error di hapus: $e', e: e, s: s);
       rethrow;
     }
   }
