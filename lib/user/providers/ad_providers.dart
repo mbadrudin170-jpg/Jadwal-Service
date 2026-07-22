1ee4f98b5a137eec021ab1e41823f85b54fff364
mbadrudin170 <mbadrudin170@gmail.com>
2026-07-22 19:41:22 +0000

    m

--- lib/user/providers/ad_providers.dart
+++ lib/user/providers/ad_providers.dart
@@ -1,12 +1,16 @@
 // path: lib/user/providers/ad_providers.dart
 
+import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:wifi/user/widget/ads/interstitial/layanan_iklan_interstisial.dart';
 
 part 'ad_providers.g.dart';
 
 @Riverpod(keepAlive: true)
-LayananIklanInterstisial interstitialAdService(Ref ref) {
+LayananIklanInterstisial? interstitialAdService(Ref ref) {
+  if (kIsWeb) {
+    return null;
+  }
   final service = LayananIklanInterstisial();
 
   ref.onDispose(service.dispose);
